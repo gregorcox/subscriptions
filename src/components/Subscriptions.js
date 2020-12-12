46db75c324ad845d39bacfc4801575ebf8e2d593
Gregor Cox <gregor@fxdigital.uk>
2020-12-12 16:32:45 +0000

    feat: map and display subscription options on page

--- src/components/Subscriptions.js
+++ src/components/Subscriptions.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import { subscriptions } from "../data/subscriptions";
+
 const Subscriptions = () => {
+    const selectSubscription = (subscription) => {
+        // TODO: Store this choice in state
+        console.log(subscription);
+    }
+
+    const options = subscriptions.map((subscription) => {
+        return (
+            <div key={subscription.id}>
+                <h3>{subscription.name}</h3>
+                <p>£{subscription.price}</p>
+                <p>{subscription.description}</p>
+                <button onClick={() => { selectSubscription(subscription)}}>Select</button>
+            </div>
+        );
+    });
+
     return (
-        <div>Hello</div>
+        <div>{options}</div>
     )
 }
 
---
+++ src/data/subscriptions.js
@@ -0,0 +1,14 @@
+export const subscriptions = [
+    {
+        id: 1,
+        name: "Standard Subscription",
+        price: 9.99,
+        description: "Choose our standard subscription with features x, y and z!"
+    },
+    {
+        id: 2,
+        name: "Premium Subscription",
+        price: 29.99,
+        description: "Choose our standard subscription with features a, b and c!"
+    }
+];
